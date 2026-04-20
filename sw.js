e06080e7755b590534606389ddbe3280dc087f2d
Saleh Jawar <140080549+Saalehj@users.noreply.github.com>
2026-04-20 15:13:01 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+    // این یک سرویس‌ورکر ساده برای فعال‌سازی دکمه نصب است
+});
