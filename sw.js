cec660c4b5a9021d841f40456a87e18b5acd41be
Saleh Jawar <140080549+Saalehj@users.noreply.github.com>
2026-04-21 12:21:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
-const CACHE_NAME = 'ronika-v6';
-const assets = ['./index.html', './manifest.json', './AppLogo.jpg'];
+const CACHE_NAME = 'ronika-v5';
+const assets = ['./', './index.html', './manifest.json', './AppLogo.jpg'];
 
 self.addEventListener('install', (e) => {
-    // نصب فوری بدون معطلی
-    self.skipWaiting();
-    
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            // ترفند هوشمند: تلاش برای دانلود فایل‌ها، اما اگر یکی خراب بود، کل نصب متوقف نشه!
-            return Promise.allSettled(assets.map(url => cache.add(url)));
-        })
-    );
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', (e) => {
-    // به هیچ وجه استریم‌های ویدیو رو کش نکن تا مرورگر هنگ نکنه
-    if (e.request.url.includes('.m3u8') || e.request.url.includes('.ts')) return;
-    
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
