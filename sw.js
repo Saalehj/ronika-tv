2e677a3f451f2e4103e28049e53041c7e082db4b
Saleh Jawar <140080549+Saalehj@users.noreply.github.com>
2026-04-21 11:52:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
-const CACHE_NAME = 'ronika-tv-v1';
+const CACHE_NAME = 'ronika-v5';
+const assets = ['./', './index.html', './manifest.json', './AppLogo.jpg'];
 
-// وقتی اپلیکیشن نصب میشه، این فایل‌ها تو حافظه گوشی ذخیره میشن
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './AppLogo.jpg'
-            ]);
-        })
-    );
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// این بخش به گوشی میگه که این یه اپلیکیشن واقعیه
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
-        })
-    );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
